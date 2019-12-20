2c15058a9596ea62e3238f5c1b172031588407cb
sodiadrhain <adesojiawobajo@gmail.com>
2019-12-20 09:23:38 +0100

    update

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="/css/master.css" type="text/css">
+    <title></title>
+  </head>
+  <body>
+    <b>Hello World</b>
+  </body>
+</html>
